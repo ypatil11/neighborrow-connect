10656f165faf7e803c2918e3c054ceeb968d8714
Yash Patil <160073551+ypatil11@users.noreply.github.com>
2025-02-23 17:33:49 -0500

    cover page updates

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,4 +1,20 @@
 
+// import Navbar from "@/components/Navbar";
+// import Hero from "@/components/Hero";
+// import FeaturedItems from "@/components/FeaturedItems";
+
+// const Index = () => {
+//   return (
+//     <div className="min-h-screen bg-white">
+//       <Navbar />
+//       <Hero />
+//       <FeaturedItems />
+//     </div>
+//   );
+// };
+
+// export default Index;
+
 import Navbar from "@/components/Navbar";
 import Hero from "@/components/Hero";
 import FeaturedItems from "@/components/FeaturedItems";
@@ -7,8 +23,21 @@ const Index = () => {
   return (
     <div className="min-h-screen bg-white">
       <Navbar />
-      <Hero />
-      <FeaturedItems />
+      <div
+        className="background-wrapper"
+        style={{
+          backgroundImage: `url('/src/assets/images/background.jpeg')`,
+        }}
+      >
+        {/* Optional Overlay */}
+        <div className="overlay"></div>
+
+        {/* Content */}
+        <div className="content">
+          <Hero />
+          <FeaturedItems />
+        </div>
+      </div>
     </div>
   );
 };
